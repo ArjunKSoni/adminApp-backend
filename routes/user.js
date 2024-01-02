9fe76ec79e7cb9116747020129bc82a51f7928c3
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2024-01-02 13:23:47 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const router = express.Router()
-const HomeUser=require("../model/homeUsers");
+const HomeUser = require("../model/homeUsers");
 
-router.post('/edit', async function (req, res){
-    const id=req.body.Id;
-    const user= await HomeUser.updateOne({_id:id},{
-        Name:req.body.Name,
-        Phone:req.body.Phone,
-        Email:req.body.Email
+router.post('/edit', async function (req, res) {
+    const id = req.body.Id;
+    const user = await HomeUser.findOneAndUpdate({ _id:id }, {
+        Name: req.body.Name,
+        Phone: req.body.Phone,
+        Email: req.body.Email
+    },{
+        new: true
     });
-    res.send({user:user})
+    res.send({ user: user })
 });
 
-router.post('/add', async function (req, res){
-    const user= new HomeUser({
-        Name:req.body.Name,
-        Phone:req.body.Phone,
-        Email:req.body.Email
+router.post('/add', async function (req, res) {
+    const user = new HomeUser({
+        Name: req.body.Name,
+        Phone: req.body.Phone,
+        Email: req.body.Email
     });
     await user.save();
-    res.send({status:true})
+    res.send({ status: true })
 });
 
-router.post("/getallusers",async(req,res)=>{
-    const data=await HomeUser.find({});
-    res.send({HomeUser:data});
+router.post("/getallusers", async (req, res) => {
+    const data = await HomeUser.find({});
+    res.send({ HomeUser: data });
 })
 
 
